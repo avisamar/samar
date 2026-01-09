aa5c8318a3c7c654969bdee2e7aee3a1e84106eb
Avinash Bhardwaj <avinash.bhardwaj@samarcapital.in>
2026-01-09 14:33:51 +0530

    logo fix

--- proxy.ts
+++ proxy.ts
@@ -22,5 +22,5 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|.*\\.(?:jpg|jpeg|png|gif|svg|ico|webp)$).*)"],
 };
